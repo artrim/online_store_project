55865a39b74495dcc56480d391d43ed3bf62a5b8
Артур Хамидуллин <artrimskiy@gmail.com>
2024-04-20 23:13:28 +0500

    correct models.py

--- catalog/models.py
+++ catalog/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
         upload_to="products/", verbose_name="Изображение", **NULLABLE,
     )
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, verbose_name="Категория", related_name="products",
+        Category, on_delete=models.CASCADE, verbose_name="Категория", related_name="products", **NULLABLE,
     )
     price = models.IntegerField(verbose_name="Цена за покупку",)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания",)
