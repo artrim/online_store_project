a8a23c746266dffa564637686ac782ef63f9501f
Артур Хамидуллин <artrimskiy@gmail.com>
2024-04-20 17:16:13 +0500

    delete 0002 migration

--- catalog/models.py
+++ catalog/models.py
@@ -32,6 +32,7 @@ class Product(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True, verbose_name="Дата последнего изменения",
     )
+    # manufactured_at = models.DateTimeField(verbose_name="Дата производства продукта", **NULLABLE)
 
     def __str__(self):
         return self.name_product
