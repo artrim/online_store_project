bae46ee4b7549a76c095a506677fcb3c2d97dc35
Артур Хамидуллин <artrimskiy@gmail.com>
2024-04-20 14:35:33 +0500

    create models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blanc': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание категории', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование товара')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    picture = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='Категория', related_name="products")
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
