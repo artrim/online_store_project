a02a83042648b97fb32d40da2b567a49ebad0eb7
Артур Хамидуллин <artrimskiy@gmail.com>
2024-04-24 22:55:05 +0500

    little corrects

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,11 +5,11 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name_product", "price", "category")
+    list_display = ("pk", "name_product", "price", "category",)
     list_filter = ("category",)
-    search_fields = ("name_product", "description_product")
+    search_fields = ("name_product", "description_product",)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name_category")
+    list_display = ("pk", "name_category",)
