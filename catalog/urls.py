50037ff99489ca9dcb1f202d729beb06a6ce722e
Артур Хамидуллин <artrimskiy@gmail.com>
2024-06-06 11:47:46 +0500

    add url for CategoryListView and cache_page for ProductDetailView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import ContactsView, ProductListView, ProductDetailView, BlogCreateView, BlogListView, \
     BlogDetailView, \
-    BlogUpdateView, BlogDeleteView, ProductCreateView, ProductUpdateView, ProductDeleteView
+    BlogUpdateView, BlogDeleteView, ProductCreateView, ProductUpdateView, ProductDeleteView, CategoryListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('product_create/', ProductCreateView.as_view(), name='product_create'),
     path('product_update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
     path('product_delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
@@ -19,4 +20,5 @@ urlpatterns = [
     path('blog_detail/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
     path('blog_update/<int:pk>', BlogUpdateView.as_view(), name='blog_update'),
     path('blog_delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
+    path('category/', CategoryListView.as_view(), name='category_list'),
 ]
